dfc43d74219a2ea17274c7ccea08079befd4bed0
Zsolt Schlachtovszky <zsolt.schlachtovszky@ekata.com>
2023-07-04 15:41:06 +0200

    highlight yanked text for a short time

--- init.lua
+++ init.lua
@@ -39,3 +39,20 @@ vim.g["aniseed#env"] = {
   module = "config.init",
   compile = true
 }
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 100,
+        })
+    end,
+})
+
+
